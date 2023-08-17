6f97659428d32d85616058e422e86346ed85783c
Toby <tobias.fatzer@bluewin.ch>
2023-08-17 16:23:19 +0000

    Fix backend linting

--- backend/main.go
+++ backend/main.go
@@ -37,25 +37,36 @@ type fortuneHandler struct {
 	store *datastore
 }
 
+// The simplified error response function
+func respondWithError(w http.ResponseWriter, status int, message string) {
+	w.WriteHeader(status)
+	_, _ = w.Write([]byte(message))
+}
+
 func (h *fortuneHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 	switch {
 	case r.Method == http.MethodGet && listFortuneRe.MatchString(r.URL.Path):
 		h.List(w, r)
-		return
 	case r.Method == http.MethodGet && getFortuneRe.MatchString(r.URL.Path):
 		h.Get(w, r)
-		return
 	case r.Method == http.MethodGet && randomFortuneRe.MatchString(r.URL.Path):
 		h.Random(w, r)
-		return
 	case r.Method == http.MethodPost && createFortuneRe.MatchString(r.URL.Path):
 		h.Create(w, r)
-		return
 	default:
-		notFound(w, r)
+		respondWithError(w, http.StatusNotFound, "not found")
+	}
+}
+
+// Simplified function for writing JSON to the response writer
+func writeJSONResponse(w http.ResponseWriter, data interface{}) {
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "internal server error")
 		return
 	}
+	_, _ = w.Write(jsonBytes)
 }
 
 func (h *fortuneHandler) List(w http.ResponseWriter, r *http.Request) {
@@ -66,13 +77,7 @@ func (h *fortuneHandler) List(w http.ResponseWriter, r *http.Request) {
 	}
 	h.store.RUnlock()
 
-	jsonBytes, err := json.Marshal(fortunes)
-	if err != nil {
-		internalServerError(w, r)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonBytes)
+	writeJSONResponse(w, fortunes)
 }
 
 func (h *fortuneHandler) Random(w http.ResponseWriter, r *http.Request) {
@@ -96,77 +101,34 @@ func (h *fortuneHandler) Random(w http.ResponseWriter, r *http.Request) {
 func (h *fortuneHandler) Get(w http.ResponseWriter, r *http.Request) {
 	matches := getFortuneRe.FindStringSubmatch(r.URL.Path)
 	if len(matches) < 2 {
-		notFound(w, r)
+		respondWithError(w, http.StatusNotFound, "not found")
 		return
 	}
 
-	if usingRedis {
-		key := matches[1]
-		val, err := dbLink.Do("hget", "fortunes", key)
-		if err != nil {
-			fmt.Println("redis hget failed", err.Error())
-		} else {
-			if val != nil {
-				msg := fmt.Sprintf("%s", val.([]byte))
-				h.store.Lock()
-				h.store.m[key] = fortune{ID: key, Message: msg}
-				h.store.Unlock()
-			}
-		}
-	}
+	key := matches[1]
 
 	h.store.RLock()
-	u, ok := h.store.m[matches[1]]
+	u, ok := h.store.m[key]
 	h.store.RUnlock()
 
 	if !ok {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("fortune not found"))
+		respondWithError(w, http.StatusNotFound, "fortune not found")
 		return
 	}
-	jsonBytes, err := json.Marshal(u)
-	if err != nil {
-		internalServerError(w, r)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonBytes)
+	writeJSONResponse(w, u)
 }
 
 func (h *fortuneHandler) Create(w http.ResponseWriter, r *http.Request) {
 	var u fortune
 	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
-		internalServerError(w, r)
+		respondWithError(w, http.StatusInternalServerError, "internal server error")
 		return
 	}
 	h.store.Lock()
 	h.store.m[u.ID] = u
 	h.store.Unlock()
 
-	if usingRedis {
-		_, err := dbLink.Do("hset", "fortunes", u.ID, u.Message)
-		if err != nil {
-			fmt.Println("redis hset failed", err.Error())
-		}
-	}
-
-	jsonBytes, err := json.Marshal(u)
-	if err != nil {
-		internalServerError(w, r)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonBytes)
-}
-
-func internalServerError(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusInternalServerError)
-	w.Write([]byte("internal server error"))
-}
-
-func notFound(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte("not found"))
+	writeJSONResponse(w, u)
 }
 
 func main() {
@@ -177,6 +139,8 @@ func main() {
 	mux.Handle("/fortunes", fortuneH)
 	mux.Handle("/fortunes/", fortuneH)
 
-	http.ListenAndServe(":9000", mux)
-
+	err := http.ListenAndServe(":9000", mux)
+	if err != nil {
+		fmt.Println("Failed to start the server:", err)
+	}
 }
