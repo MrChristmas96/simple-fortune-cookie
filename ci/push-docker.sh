fc901b987e47a8f8980bd110694e2a526e22a8ef
Toby <tobias.fatzer@bluewin.ch>
2023-08-17 13:36:59 +0000

    update pull/push container script

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 docker_username_lower=$(echo "$docker_username" | tr '[:upper:]' '[:lower:]')
+repository_name="mrchristmas96/simple-fortune-cookie"
 echo "$docker_password" | docker login ghcr.io --username "$docker_username_lower" --password-stdin
-docker push "ghcr.io/$docker_username_lower/simple-fortune-cookie-backend:1.0-${GIT_COMMIT::8}"
-docker push "ghcr.io/$docker_username_lower/simple-fortune-cookie-frontend:1.0-${GIT_COMMIT::8}"
-docker push "ghcr.io/$docker_username_lower/simple-fortune-cookie-backend:latest"
-docker push "ghcr.io/$docker_username_lower/simple-fortune-cookie-frontend:latest"
+docker push "ghcr.io/$repository_name/simple-fortune-cookie-backend:1.0-${GIT_COMMIT::8}"
+docker push "ghcr.io/$repository_name/simple-fortune-cookie-frontend:1.0-${GIT_COMMIT::8}"
+docker push "ghcr.io/$repository_name/simple-fortune-cookie-backend:latest"
+docker push "ghcr.io/$repository_name/simple-fortune-cookie-frontend:latest"
 wait
